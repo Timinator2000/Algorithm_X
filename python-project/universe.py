3458c70e1bcccd27f5f943379f527a2e44f9634a
Timinator <74882103+TimSzymanski@users.noreply.github.com>
2024-05-14 16:44:15 -0600

    Update universe.py

--- python-project/universe.py
+++ python-project/universe.py
@@ -1,5 +1,16 @@
 from AlgorithmX import AlgorithmXSolver
 
+class MySolver(AlgorithmXSolver):
+
+    def __init__(self):
+        requirements = [('requirement 1', 5), ('requirement 2', 5)]
+        actions = {
+            ('Cover Req 1', 5):[('requirement 1', 5)],
+            ('Cover Req 2', 4):[('requirement 2', 5)]
+        }
+
+        super().__init__(requirements, actions)
+
 def count_all_stars(galaxies):
     total_stars = 0
     for stars in galaxies:
