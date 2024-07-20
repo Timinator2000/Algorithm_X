11407f25f14480683ed5828b8b445f6663c1eaaa
Timinator <74882103+TimSzymanski@users.noreply.github.com>
2024-07-20 12:13:09 -0600

    Update part_I_generate_solutions.py

--- python-project/part_I_generate_solutions.py
+++ python-project/part_I_generate_solutions.py
@@ -1,18 +1,54 @@
 # TECH.IO allows me to use this import statement to make my examples concise and easy
-# to study. It is highly likely you will need to copy all of the AlgorithmXSolver
-# code into your coding environment.
+# to study. Unless your coding environment will let you create an AlgorithmX package,
+# you will need to copy all of the AlgorithmXSolver code into your coding environment.
 
 from AlgorithmX import AlgorithmXSolver
 
 class MrsKnuthPartISolver(AlgorithmXSolver):
 
     def __init__(self):
-        requirements = [('requirement 1', ), ('requirement 2', )]
-        actions = {
-            ('Cover Req 1', ):[('requirement 1', )],
-            ('Cover Req 2', ):[('requirement 2', )]
-        }
+        
+        requirements = [(‘student scheduled’, ‘Ayla’),
+                        (‘student scheduled’, ‘Bob’),
+                        (‘student scheduled’, ‘Alex’),
+                        (‘slot filled’, ‘Th’, 2),
+                        (‘slot filled’, ‘Th’, 3),
+                        (‘slot filled’, ‘Th’, 4),
+                        (‘instrument on day’, ‘Th’, ‘Trumpet’),
+                        (‘instrument on day’, ‘Th’, ‘Drums’),
+                        (‘instrument on day’, ‘Th’, ‘Tuba’)]
+        
+        actions = dict()
 
+        action = (‘place student’, ‘Ayla’, ‘Trumpet’, ‘Th’, 2)
+        actions[action] = [(‘student scheduled’, ‘Ayla’),
+                            (slot filled’, ‘Th’, 2),
+                            (‘instrument on day’, ‘Th’, ‘Trumpet’)]
+        
+        action = (‘place student’, ‘Bob’, ‘Drums’, ‘Th’, 2)
+        
+        actions[action] = [(‘student scheduled’, ‘Bob’),
+                            (‘slot filled’, ‘Th’, 2),
+                            (‘instrument on day’, ‘Th’, ‘Drums’)]
+
+        actions = (‘place student’, ‘Bob’, ‘Drums’, ‘Th’, 3)
+        
+        actions[action] = [(‘student scheduled’, ‘Bob’),
+                            (‘slot filled’, ‘Th’, 3),
+                            (‘instrument on day’, ‘Th’, ‘Drums’)]
+
+        actions = (‘place student’, ‘Alex’, ‘Drums’, ‘Th’, 2)
+        
+        actions[action] = [(‘student scheduled’, ‘Alex’),
+                            (‘slot filled’, ‘Th’, 2),
+                            (‘instrument on day’, ‘Th’, ‘Tuba’)]
+
+        actions = (‘place student’, ‘Alex’, ‘Drums’, ‘Th’, 4)
+        
+        actions[action] = [(‘student scheduled’, ‘Alex’)
+                            (‘slot filled’, ‘Th’, 4)
+                            (‘instrument on day’, ‘Th’, ‘Tuba’)]
+        
         super().__init__(requirements, actions)
 
 
