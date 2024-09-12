46f35097d640ef3eb1a188bbc19161bb5413d59c
Timinator <74882103+Timinator2000@users.noreply.github.com>
2024-09-12 11:59:10 -0600

    Update AlgorithmX.py
    Removed the August 13th changes and updated comments. Specifically removed the functionality that allowed rows to be preselected before running Algorithm X. The only option allowed now is to restrict the rows so that Algorithm X has no choice but to select rows that must be part of the solution, such as prefilled cells in a logic puzzle.

--- python-project/AlgorithmX.py
+++ python-project/AlgorithmX.py
@@ -1,7 +1,9 @@
 #  This solution uses Knuth's Algorithm X and his Dancing Links (DLX):
-#  (DLX Based Algorithm X Solver Last Revised 08/02/2024)
+#  (DLX Based Algorithm X Solver Last Revised 09/12/2024)
 #
-#   https://en.wikipedia.org/wiki/Knuth%27s_Algorithm_X
+#  For a detailed explanation and tutorial, please see my 
+#  Algorithm X playgound on Tech.io.
+#
 #
 #  June 02, 2024 - history added to allow subclasses to make adaptations for multiplicity.
 #                - giving this Solver similar functionality to Knuth's Algorithm M.
@@ -18,46 +20,15 @@
 #                    Algorithm X uses DLX to select rows by node. A subclass would have
 #                    a hard time knowing about nodes, whereas selecting a row by action
 #                    is straightforward.
-
-#  To study the implementation of Algorithm X, I originally used the following write-up
-#  by Ali Assaf (ali.assaf.mail@gmail.com) titled "Algorithm X in 30 Lines!" 
 #
-#    https://www.cs.mcgill.ca/~aassaf9/python/algorithm_x.html
+#  Sept 02, 2024 - Significant speed increase by changing O to a set and skipping
+#                - optional requirements when looking for next column to cover. 
 #
-#  Assaf's Algorithm X implementation is not DLX based, but it was extremely helpful in my
-#  journey that resulted in the DLX based solver below.
-#
-#  To understand this DLX based solver will require what I consider mind-bending study
-#  of Knuth's Dancing Links. I believe I was only able to become proficient with DLX
-#  because I was able to study @RoboStac's solution to Constrained Latin Squares on
-#  www.codingame.com.
-#
-#  https://www.codingame.com/training/medium/constrained-latin-squares
-#
-#
-#  Ultimately, I have used @RoboStac's DLXCell class which represents a single cell in
-#  Algorithm X's Matrix A and provides all the "dancing" functionality. In a much less
-#  significant effort, I took @Robostac's Algorithm X logic and reworked it to fit the
-#  Algorithm X logic laid out above by Assaf. In the end, I created what I believe to 
-#  be an extremly reusable DLX based Algorithm X solver with significant options for
-#  customization.
-#
-#  If you are interested in Algorithm X and Dancing Links, I invite you to copy this code
-#  and try to solve all the puzzles on @5DN1L's Algorithm X list below. You should be able
-#  to do every puzzle without making a single change to my DLX based AlgorithmXSolver class.
-#  Simply create a subclass Solver and add the puzzle specific details...which many times is
-#  a very challenging task itself!
-#
-#  @5DN1L's Puzles Solvable with Algorithm X:
-#
-#  https://www.codingame.com/forum/t/puzzles-solvable-by-algorithm-x-dancing-links/196871
+#  Sept 12, 2024 - Removed the August 13th changes and updated comments. 
 #
 
-
-# On to the solver...
-
-# DLXCell is one cell in Matrix A. This implementation was mostly copied 
-# from @RoboStac's solution to Constrained Latin Squares on Codingame.com.
+# DLXCell is one cell in the Algorithm X matrix. This implementation was mostly
+# copied from @RoboStac's solution to Constrained Latin Squares on Codingame.com.
 #
 # https://www.codingame.com/training/medium/constrained-latin-squares
 #
@@ -168,10 +139,10 @@ class AlgorithmXSolver():
     #
     def __init__(self, R: list, A: dict, O: list = []):
         self.A  = A
-        self.R  = R + O
-        self.O  = O
+        self.R  = R + list(O)
+        self.O  = set(O)
 
-        # The list of actions (rows) that produce the current path through Matrix A.
+        # The list of actions (rows) that produce the current path through the matrix..
         self.solution = []
         self.solution_count = 0
         
@@ -182,10 +153,10 @@ class AlgorithmXSolver():
 
         # For the basic Algorithm X Solver, all solutions are always valid.  However, a subclass
         # can add functionality to check solutions as they are being built to steer away from
-        # invalid solutions.  The basic Algorithm X Solver never modifies this attribue in any way.
+        # invalid solutions.  The basic Algorithm X Solver never modifies this attribue.
         self.solution_is_valid = True
 
-        # Create a column in Matrix A for every requirement.
+        # Create a column in the matrix for every requirement.
         self.matrix_a_root = DLXCell()
         self.matrix_a_root.size = 10000000
         self.matrix_a_root.title = 'root'
@@ -201,7 +172,7 @@ class AlgorithmXSolver():
         for i in range(len(self.col_headers)):
             self.matrix_a_root.attach_horiz(self.col_headers[i])
 
-        # Create a row in Matrix A for every action.
+        # Create a row in the matrix for every action.
         for action in self.A:
             previous_cell = None
             for requirement in A[action]:
@@ -239,7 +210,12 @@ class AlgorithmXSolver():
                 if best_column == self.matrix_a_root or value < best_value:
                     best_column = node
                     best_value  = value
-            node = node.next_x
+                node = node.next_x
+
+            else:
+
+                # Optional requirements stop the search for the best column.
+                node = self.matrix_a_root
             
         if best_column == self.matrix_a_root:
             self._process_solution()
@@ -262,7 +238,7 @@ class AlgorithmXSolver():
                 
             # Loop through the possible actions sorted by the given sort criteria. A basic
             # Algorithm X implementation does not provide sort criteria. Actions are tried
-            # in the order they happen to occur in Matrix A.
+            # in the order they happen to occur in the matrix.
             for node in sorted(actions, key=lambda a:self._action_sort_criteria(a)):
                 self.select(node=node)
                 if self.solution_is_valid:
@@ -277,42 +253,14 @@ class AlgorithmXSolver():
 
     # Algorithm X Step 4 - Details:
     #
-    # The select method updates Matrix A when a row is selected as part of a solution path.
+    # The select method updates the matrix when a row is selected as part of a solution.
     # Other rows that satisfy overlapping requirements need to be deleted and in the end,
-    # all columns satisfied by the selected row get removed from Matrix A.
-    def select(self, node=None, action=None):
+    # all columns satisfied by the selected row get removed from the matrix.
+    def select(self, node):
 
-        if node:
-            node.select()
-            self.solution.append(node.row_header.title)
-            self._process_row_selection(node.row_header.title)
-
-        # This is only used to preselect rows and make them part of the solution. This should
-        # be done before the backtracking and cannot be undone. It is meant for problems that
-        # have partial solutions, such as a partially prefilled Sudoku.
-        elif action:
-
-            finished = False
-
-            # Loop through all columns.
-            col_node = self.matrix_a_root.next_x
-            while not finished and col_node != self.matrix_a_root:
-
-                # Loop through all the rows, looking for a cell with proper header title.
-                row_node = col_node.next_y
-                while not finished and row_node != col_node:
-                    if row_node.row_header.title == action:
-                        row_node.select()
-                        self.solution.append(action)
-                        self._process_row_selection(action)
-                        finished = True
-
-                    row_node = row_node.next_y                
-                
-                col_node = col_node.next_x
-                
-            if not finished:
-                print(f'ALGO X NEVER FOUND A WAY TO SELECT {action}.\n', file=sys.stderr, flush=True)
+        node.select()
+        self.solution.append(node.row_header.title)
+        self._process_row_selection(node.row_header.title)
 
 
     # Algorithm X Step 4 - Clean Up:
@@ -320,18 +268,12 @@ class AlgorithmXSolver():
     # The select() method selects a row as part of the solution being explored.  Eventually that
     # exploration ends and it is time to move on to the next row (action).  Before moving on,
     # Matrix A and the partial solution need to be restored to its prior state.
-    def deselect(self, node=None, action=None):
+    def deselect(self, node):
 
-        if node:
-            node.unselect()
-            self.solution.pop()
-            self._process_row_deselection(node.row_header.title)
+        node.unselect()
+        self.solution.pop()
+        self._process_row_deselection(node.row_header.title)
 
-        # This is not currently allowed. Selecting by action can be used to preselect rows
-        # before starting the backtracking, but those rows cannot be deselected.
-        if action:
-            pass
-        
 
     # In cases of multiplicity, this method can be used to ask Algorithm X to remember that
     # it has already tried certain things. For instance, if Emma wants two music lessons per
@@ -345,9 +287,9 @@ class AlgorithmXSolver():
             self.history[-1].add((item_to_remember))
 
         
-    # In some cases it may be beneficial to have Algorithm X try certain paths through Matrix A.
+    # In some cases it may be beneficial to have Algorithm X try certain paths through the matrix.
     # This can be the case when there is reason to believe certain actions have a better chance than
-    # other actions at producing complete paths through Matrix A.  The method included here does
+    # other actions at producing complete paths through the matrix. The method included here does
     # nothing, but can be overridden in the case a subclass wishes to influence the order in which
     # Algorithm X tries rows (actions) that cover some particular column.
     def _action_sort_criteria(self, node):
@@ -355,9 +297,9 @@ class AlgorithmXSolver():
     
 
     # In some cases it may be beneficial to have Algorithm X try covering certain requirements
-    # before others as it looks for paths through Matrix A.  The default is to sort the requirements
+    # before others as it looks for paths through the matrix. The default is to sort the requirements
     # by how many actions cover each requirement, but in some case there might be several 
-    # requirements covered by the same number of actions.  By overriding this method, the
+    # requirements covered by the same number of actions. By overriding this method, the
     # Algorithm X Solver can be directed to break ties a certain way or consider some other way
     # of prioritizing the requirements.
     def _requirement_sort_criteria(self, node):
@@ -365,34 +307,28 @@ class AlgorithmXSolver():
     
     
     # The following method can be overridden by a subclass to add logic to perform more detailed solution
-    # checking if invalid paths are possible through Matrix A.  Some problems have requirements that
-    # cannot be captured in the basic requirements list passed into the __init__() method.  For instance,
+    # checking if invalid paths are possible through the matrix. Some problems have requirements that
+    # cannot be captured in the basic requirements list passed into the __init__() method. For instance,
     # a solution might only be valid if it fits certain parameters that can only be checked at intermediate
-    # steps.  In a case like that, this method can be overridden to add the functionality necessary to 
+    # steps. In a case like that, this method can be overridden to add the functionality necessary to 
     # check the solution.
     #
     # If the subclass logic results in an invalid solution, the 'solution_is_valid' attribute should be set
-    # to False instructing the Algorithm X to stop progressing down this path in Matrix A.
+    # to False instructing Algorithm X to stop progressing down this path in the matrix.
     def _process_row_selection(self, row):
         pass
 
 
     # This method can be overridden by a subclass to add logic to perform more detailed solution
-    # checking if invalid paths are possible through Matrix A.  This method goes hand-in-hand with the
-    # _process_row_selection() method above.
-    #
-    # If the subclass logic results in an invalid solution, the 'solution_is_valid' attribute should be
-    # set to False instructing Algorithm X to stop progressing down this path in Matrix A.  When the
-    # Algorithm X backtracking comes back to deselect this row, the 'solution_is_valid' attribute must 
-    # be reset to True. That should be done here.
+    # checking if invalid paths are possible through the matrix. This method goes hand-in-hand with the
+    # _process_row_selection() method above to "undo" what was done above.
     def _process_row_deselection(self, row):
         pass
 
 
     # This method can be overridden to instruct Algorithm X to do something every time a solution is found.
-    # For example, this method could be updated to simply count the number of solutions. It's also possible 
-    # that not all full paths through Matrix A are truly valid, but maybe that cannot be determined until 
-    # the very end. In that case, logic could be added here to perform some checking on a solution Algorithm X 
-    # considers valid, but ultimate validity requires the solution passing some last bit of logic.
+    # For instance, Algorithm X might be looking for the best solution or maybe each solution must be
+    # validated in some way. In either case, the solution_is_valid attribute can be set to False
+    # if the current solution should not be considered valid and should not be generated.
     def _process_solution(self):
         pass
