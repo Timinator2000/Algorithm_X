72837670c9e8ad20578437021433502ce6eb41d6
Timinator <74882103+Timinator2000@users.noreply.github.com>
2024-09-14 07:32:45 -0600

    Update part_II_solver_test.py

--- python-project/part_II_solver_test.py
+++ python-project/part_II_solver_test.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     main_program()
     channel = f'{random.choice(CONGRATS)} 🌟'
     
-    message = 'We live in the age of Artificial Intelligence and the first rule of AI is, ' +
-              '“Show me the data!” In a different, but similar way, Algorithm X depends ' +
-              'on data. As you find more and more information to pass to Algorithm X, ' +
-              'mostly in the form of requirements, Algorithm X will make better and faster decisions.' +
-              '\n' +
-              'Identifying requirements for mutual exclusivity can significantly boost the performance ' +
-              'of Algorithm X. For some problems, it isn’t even important to identify all the ' +
-              'mutual exclusivity. Simply identifying <u>some</u> of the mutual exclusivity ' +
+    message = 'We live in the age of Artificial Intelligence and the first rule of AI is, ' + \
+              '“Show me the data!” In a different, but similar way, Algorithm X depends ' + \
+              'on data. As you find more and more information to pass to Algorithm X, ' + \
+              'mostly in the form of requirements, Algorithm X will make better and faster decisions.' + \
+              '\n' + \
+              'Identifying requirements for mutual exclusivity can significantly boost the performance ' + \
+              'of Algorithm X. For some problems, it isn’t even important to identify all the ' + \
+              'mutual exclusivity. Simply identifying <u>some</u> of the mutual exclusivity ' + \
               'gives Algorithm X the intelligence necessary to refine its search and produce results much quicker.'
     
    
