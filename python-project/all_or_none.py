a0fe1248f050c70db70b2b9be8921e079920a014
Timinator <74882103+Timinator2000@users.noreply.github.com>
2024-11-27 19:41:11 -0700

    Update all_or_none.py

--- python-project/all_or_none.py
+++ python-project/all_or_none.py
@@ -1,4 +1,6 @@
-def minimize_all_or_none_sets(all_or_none_groups):
+from Typing import List, Tuple
+
+def minimize_all_or_none_sets(all_or_none_groups: List[Tuple[str]]):
 
     # Combine any groups that can be combined and return
     # a minimum number of groups.
